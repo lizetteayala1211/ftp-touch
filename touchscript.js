afd4efdc7b3154286c8a2301879452e5dcf48af4
Lizette Ayala <ayall291@newschool.edu>
2021-07-11 17:53:23 -0400

    Update touchscript.js

--- touchscript.js
+++ touchscript.js
@@ -2356,13 +2356,13 @@ $( window ).on( "load", function() {
   $( "#nav-top-link-two" ).delay( 2000 ).animate({fontSize: "1vh" }, 400 );
   $( "#nav-top-link-three" ).delay( 2000 ).animate({fontSize: "1vh" }, 400 );
   $( "#nav-middle-copy-section" ).delay( 2000 ).animate({ 'marginTop' : "5px" }, 400 );
-  $( "#nav-middle-copy" ).delay( 2000 ).animate({fontSize: "4vh" }, 400 );
-  $( "#nav-bottom-copy" ).delay( 2000 ).animate({fontSize: "1vh" }, 400 );
-  $( "#nav-bottom-copy-two" ).delay( 2000 ).animate({fontSize: "1vh" }, 400 );
-  $( "#nav-bottom-copy-three" ).delay( 2000 ).animate({fontSize: "1vh" }, 400 );
+  $( "#nav-middle-copy" ).delay( 2000 ).animate({fontSize: "6vh" }, 400 );
+  $( "#nav-bottom-copy" ).delay( 2000 ).animate({fontSize: "1.2vh" }, 400 );
+  $( "#nav-bottom-copy-two" ).delay( 2000 ).animate({fontSize: "1.2vh" }, 400 );
+  $( "#nav-bottom-copy-three" ).delay( 2000 ).animate({fontSize: "1.2vh" }, 400 );
   $( "#top-part" ).delay( 2000 ).animate({ height: "13vh" }, 400 );
-  $( "#bottom-part" ).delay( 2000 ).animate({ top: "13.1vh" }, 400 );
-  $( "#bottom-part" ).delay( 800 ).animate({ height: "75vh" }, 400 );
+  $( "#bottom-part" ).delay( 2000 ).animate({ top: "12.5vh" }, 400 );
+  $( "#bottom-part" ).delay( 800 ).animate({ height: "70vh" }, 400 );
 });
 
 
@@ -2386,6 +2386,10 @@ $( window ).on( "load", function() {
 
     }
  });
+
+
+
+
 
 
 
